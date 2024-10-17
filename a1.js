e7d583a3742da5bbd977092aeca3b8082cce6233
Abdul Rehman <ar03353505910@gmail.com>
2024-10-17 15:27:31 +0500

    update

---
+++ a1.js
@@ -0,0 +1,7 @@
+alert("Welcome to our website!");
+alert("please enter a valid password!");
+alert("welcome to js land...\nhappy coding");
+alert("welcome to js land");
+alert("happy coding...\n\n prevent this page from creating additional dialogs");
+alert("hello...i can run js through my web browser's console")
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
---
+++ a2.js
@@ -0,0 +1,56 @@
+//        .... QUESTION 1 ....
+let username;
+
+        username = "abdul-rehman";
+
+         alert("Welcome, " + username + "!");
+
+
+
+//           .... QUESTION 2 ....
+ var myName = "abdul-rehman"
+
+ alert("Hello, my name is " + myName + "!");
+
+//       .... QUESTION 3 ....
+ var message;
+
+ message = "Hello World";
+
+ alert(message);
+
+
+       
+//         .... QUESTION 4 ....
+ var studentName = "abdul-rehman";
+ var studentAge = 30;
+ var studentCertification = "Certified Mobile App Development";
+
+ var message = "Name: " + studentName + "\n" +
+               "Age: " + studentAge + " Years Old\n" +
+               "Certification: " + studentCertification;
+
+ alert(message);
+
+//      .... QUESTION 5 ....
+
+ var pizzaMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+ alert(pizzaMessage);
+
+//       .... QUESTION 6 ....
+ var email = "example@example.com";
+ var message = "My Email Address is " + email;
+ alert(message);
+
+//      .... question 7 ....
+
+ var book = "A Smarter way to learn JavaScript";
+ alert("I am learning from the book: " + book);
+
+//         .... QUESTION 8 ....
+ document.write("Yah! I can write HTML content through JavaScript");
+
+//           .... QUESTION 9 ....
+var myString = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+    alert(myString);
+    document.write(myString);
